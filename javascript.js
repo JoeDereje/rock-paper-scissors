ee0057891c165b0b884d944d5a7c64da28eb7d2c
JoeDereje <dtesfaye63@gmail.com>
2024-01-16 00:37:18 +0300

    Add feature for accepting user input and display single round result of the game

--- javascript.js
+++ javascript.js
@@ -8,12 +8,35 @@ function getComputerChoice() {
     
     //check and return computer's actual selection from the possible choices
     if(computerChoice == 0) {
-        return possibleChoices[computerChoice].toLowerCase();
+        return possibleChoices[computerChoice];
     }
     else if(computerChoice == 1) {
-        return possibleChoices[computerChoice].toLowerCase();
+        return possibleChoices[computerChoice];
     }
     else {
-        return possibleChoices[computerChoice].toLowerCase();
+        return possibleChoices[computerChoice];
+    }
+}
+//Accept user input
+let playerSelection = prompt("Enter your choice");
+
+//Determines the winner between the user and computer based on game logic.
+function singleRoundRPS(playerSelection, computerSelection) {
+    //a variable to store the lower case of user selection.
+    let lcPlayerSelection = playerSelection.toLowerCase();
+    
+    let userWin = "You win, Congratulation.";
+    let compWin = "Computer win";
+    let draw = "It's a draw, please play again!";
+
+    //Game logic to determine the winner
+    if(lcPlayerSelection == "rock" && computerSelection == "scissors" || lcPlayerSelection == "paper" && computerSelection == "rock" || lcPlayerSelection == "scissors" && computerSelection == "paper") {
+        return userWin;
+    }
+    else if(lcPlayerSelection == "rock" && computerSelection == "paper" || lcPlayerSelection == "paper" && computerSelection == "scissors" || lcPlayerSelection == "scissors" && computerSelection == "rock") {
+        return compWin;
+    }
+    else {
+        return draw;
     }
 }
